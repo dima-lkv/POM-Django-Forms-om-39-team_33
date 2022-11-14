2cbeae6aa31a62db10fa53dafa5145fb25484de6
Pavlo Tkachenko <pvt0701@gmail.com>
2022-11-14 22:34:49 +0200

    -add register

--- library/authentication/urls.py
+++ library/authentication/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path, include
 from . import views
 
+app_name = 'authentication'
+
 urlpatterns = [
     path('', views.startPage),
-    path('register/', views.register_page),
-    path('login/', views.login_page),
+    path('register/', views.register, name="register"),
+    path('login/', views.login, name="login"),
 
 ]
--- library/authentication/views.py
+++ library/authentication/views.py
@@ -1,13 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from authentication.models import CustomUser
 
 
 def startPage(request):
     return render(request, 'authentication/home.html')
 
 
-def register_page(request):
+def register(request):
+    if request.method == 'POST':
+        data = {label: data for label, data in request.POST.items() if label != 'csrfmiddlewaretoken' and data}
+        user = CustomUser.objects.create_user(**data)
+        if user:
+            return redirect("authentication:login")
     return render(request, 'authentication/reg.html')
 
 
-def login_page(request):
+def login(request):
     return render(request, 'authentication/log.html')
