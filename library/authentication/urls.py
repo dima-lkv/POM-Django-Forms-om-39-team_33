311d46f5e20c38619a86c2f3bdb23c1e62eaedeb
dima-lkv <ilkiv.dima15@gmail.com>
2022-11-14 15:03:02 +0200

    frontend done

--- library/authentication/urls.py
+++ library/authentication/urls.py
@@ -2,5 +2,8 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.startPage)
+    path('', views.startPage),
+    path('register/', views.register_page),
+    path('login/', views.login_page),
+
 ]
--- library/authentication/views.py
+++ library/authentication/views.py
@@ -2,4 +2,12 @@ from django.shortcuts import render
 
 
 def startPage(request):
-    return render(request, 'authentication/base.html')
+    return render(request, 'authentication/home.html')
+
+
+def register_page(request):
+    return render(request, 'authentication/reg.html')
+
+
+def login_page(request):
+    return render(request, 'authentication/log.html')
