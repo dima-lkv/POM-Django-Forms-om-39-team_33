5a07ffd63b57afb59547b751c2145da5e91fa343
Pavlo Tkachenko <pvt0701@gmail.com>
2022-11-16 16:53:02 +0200

    - add user table

--- library/authentication/urls.py
+++ library/authentication/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('register/', views.registerPage, name='register'),
     path('login/', views.loginPage, name='login'),
     path('logout/', views.logoutUser, name='logout'),
+    path('user/', views.pageUser, name='user'),
     path('author/', include('author.urls')),
     path('book/', include('book.urls')),
     path('order/', include('order.urls')),
